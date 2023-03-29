df4aaeea3db2c972072db8557c396256490d3f00
Andres Cordova <andres.cordova@telusinternational.com>
2023-03-28 21:30:15 -0600

    Adding cart modal.

--- section-11-food-project/src/App.js
+++ section-11-food-project/src/App.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShow, setCartIsShow] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShow(false);
+  }
   return (
     <>
-      <Header />
+      {cartIsShow && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
