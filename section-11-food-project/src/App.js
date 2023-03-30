91e04e07bb334908ab25679f34e0ce54939c89e0
Andres Cordova <andres.cordova@telusinternational.com>
2023-03-29 18:06:26 -0600

    adding provider

--- section-11-food-project/src/App.js
+++ section-11-food-project/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShow, setCartIsShow] = useState(false);
@@ -13,13 +14,13 @@ function App() {
     setCartIsShow(false);
   }
   return (
-    <>
+    <CartProvider>
       {cartIsShow && <Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
