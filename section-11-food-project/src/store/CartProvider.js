9f671c6301709b9ae3c8f1edaf29ceba32bf9adf
Andres Cordova <andres.cordova@telusinternational.com>
2023-03-29 21:53:05 -0600

    Iterative data and consume them on the components.

--- section-11-food-project/src/store/CartProvider.js
+++ section-11-food-project/src/store/CartProvider.js
@@ -1,22 +1,50 @@
-import React from 'react'
-import CartContext from './cart-context'
+import React, { useReducer } from "react";
+import CartContext from "./cart-context";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
 
 const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
-    const addItemToCartHandler = (item) => {};
-    const removeItemCartHandler = (item) => {}
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
 
-    const cartContext = {
-        items: [],
-        totalAmount: 0,
-        addItem: addItemToCartHandler,
-        removeItem: removeItemCartHandler
-    };
+  const removeItemCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemCartHandler,
+  };
   return (
     <CartContext.Provider value={cartContext}>
-        {props.children}
+      {props.children}
     </CartContext.Provider>
-  )
-}
+  );
+};
 
-export default CartProvider
+export default CartProvider;
